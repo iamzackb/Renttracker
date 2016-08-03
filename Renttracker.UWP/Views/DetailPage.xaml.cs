57eb210201305914f35b0ed35e3bf55ecaa642af
Callum Moffat <callum90ish@gmail.com>
2016-08-03 14:12:44 +1200

    Initial commit

---
+++ Renttracker.UWP/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Renttracker.UWP.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Renttracker.UWP.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
