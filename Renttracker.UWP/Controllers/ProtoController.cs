24e5c51ceec24a57189574786704075e3cdea020
Shawn Beachy <Shawn Beachy>
2016-08-03 08:34:10 -0400

    Gets some sample homes and filtering in place.

---
+++ Renttracker.UWP/Controllers/ProtoController.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using Renttracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Renttracker.UWP.Controllers
+{
+    public static class ProtoController
+    {
+        public static async Task<IEnumerable<Home>> GetHomesFromSampleJsonAsync()
+        {
+            var homes = new List<Home>();
+            var homesFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///homes.json"));
+            var homesJson = await FileIO.ReadTextAsync(homesFile);
+            homes = JArray.Parse(homesJson).ToObject<List<Home>>();
+            return homes;
+        }
+    }
+}
--- Renttracker.UWP/ViewModels/MainPageViewModel.cs
+++ Renttracker.UWP/ViewModels/MainPageViewModel.cs
@@ -1,22 +1,49 @@
 using Template10.Mvvm;
 using System.Collections.Generic;
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Template10.Services.NavigationService;
 using Windows.UI.Xaml.Navigation;
+using MyToolkit.Collections;
+using Renttracker.Models;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Renttracker.UWP.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        MtObservableCollection<Home> _homes { get; set; }
+        public ObservableCollectionView<Home> Homes { get; set; }
+
+        int _minPrice = 0;
+        public int MinPrice { get { return _minPrice; } set { Set(ref _minPrice, value); FilterHomes(); } }
+
+        int _maxPrice = 10000;
+        public int MaxPrice { get { return _maxPrice; } set { Set(ref _maxPrice, value); FilterHomes(); } }
+
+        int _minBeds = 1;
+        public int MinBeds { get { return _minBeds; } set { Set(ref _minBeds, value); FilterHomes(); } }
+
+        int _maxBeds = 5;
+        public int MaxBeds { get { return _maxBeds; } set { Set(ref _maxBeds, value); FilterHomes(); } }
+
+        Func<Home, bool> PriceFilter;
+        Func<Home, bool> BedsFilter;
+
+        List<Func<Home, bool>> Filters = new List<Func<Home, bool>>();
+
         public MainPageViewModel()
         {
-
+            _homes = new MtObservableCollection<Home>();
+            Homes = new ObservableCollectionView<Home>(_homes);
+            PriceFilter = a => a.Price <= MaxPrice && a.Price >= MinPrice;
+            BedsFilter = a => a.Beds <= MaxBeds && a.Beds >= MinBeds;
         }
         
         public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> suspensionState)
         {
+            _homes.AddRange(await Controllers.ProtoController.GetHomesFromSampleJsonAsync());
             await Task.CompletedTask;
         }
 
@@ -30,6 +57,56 @@ namespace Renttracker.UWP.ViewModels
             args.Cancel = false;
             await Task.CompletedTask;
         }
+
+        public void PriceToggleSwitchToggled(object sender, RoutedEventArgs e)
+        {
+            var toggleSwitch = sender as ToggleSwitch;
+
+            if (toggleSwitch.IsOn)
+                AddPriceFilter();
+            else
+                RemovePriceFilter();
+
+            FilterHomes();
+        }
+
+        public void BedsToggleSwitchToggled(object sender, RoutedEventArgs e)
+        {
+            var toggleSwitch = sender as ToggleSwitch;
+
+            if (toggleSwitch.IsOn)
+                AddBedsFilter();
+            else
+                RemoveBedsFilter();
+
+            FilterHomes();
+        }
+
+        void FilterHomes()
+        {
+            Homes.Filter = a =>
+            {
+                var matches = true;
+                Filters.ForEach(b =>
+                {
+                    if (!b.Invoke(a).Equals(true))
+                        matches = false;
+                });
+                return matches;
+            };
+        }
+
+        void AddPriceFilter() =>
+            Filters.Add(PriceFilter);
+
+        void RemovePriceFilter() =>
+            Filters.Remove(PriceFilter);
+
+        void AddBedsFilter() =>
+            Filters.Add(BedsFilter);
+
+        void RemoveBedsFilter() =>
+            Filters.Remove(BedsFilter);
     }
 }
 
